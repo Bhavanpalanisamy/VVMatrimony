7de91a181a43434b3df0426f4991b070494f71d2
Nivaskumaravel <kumaravelnivas@gmail.com>
2024-05-03 17:44:31 +0530

    Committing the base for the project

---
+++ lib/Common_Widgets/Common_Button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../utilits/ApiService.dart';
+import '../utilits/Common_Colors.dart';
+import '../utilits/Text_Style.dart';
+import 'Image_Path.dart';
+
+
+
+// ELEVATED BUTTON
+Widget CommonElevatedButton(
+  BuildContext context,
+  String titleName,
+  void Function()? onPress,
+){
+  return ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: blue3,
+      minimumSize: Size(double.infinity, 50),
+      elevation: 9,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+    onPressed: onPress,
+    child: Text(
+      titleName,
+      style: ButtonT,
+    ),
+  );
+}
+
+
+
+
+
+
---
+++ lib/Common_Widgets/Image_Path.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+// PNG IMAGE PATH
+Widget ImgPathPng(String pathPNG){
+  return  Image(image: AssetImage("lib/assets/${pathPNG}"));
+}
+
+
+// SVG IMAGE PATH
+
+Widget ImgPathSvg(String pathSVG){
+  return  SvgPicture.asset("lib/assets/${pathSVG}");
+}
+
+// Logo Image
+ Widget LoginScreenImage(){
+  return ImgPathSvg("LoginscreenImg.svg");
+ }
+Widget Logo(){
+  return ImgPathSvg("logo.svg");
+}
+
+
